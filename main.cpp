dc3c2141b393a9c86f87358ec5d65dce8ac98e69
meysam <mysm.shahbazi@gmail.com>
2022-09-20 16:01:10 +0430

    create project

--- main.cpp
+++ main.cpp
@@ -1,3 +1,28 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <numeric>
+#include <chrono>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <dirent.h>
+#include "NvInfer.h"
+#include "cuda_runtime_api.h"
+#include "logging.h"
+#include "BYTETracker.h"
+
+
+
+
+
+
+
+int main(int argc, char** argv) 
+{
+
+
+    return 0;
+}
 
 
 
